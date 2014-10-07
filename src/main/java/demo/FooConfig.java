7e4ee9d492fa6af6f6e2ded5a0937a17737391f2
Mariusz Smykuła <mariuszs@gmail.com>
2014-10-07 22:13:29 +0200

    Fixed!

--- src/main/java/demo/FooConfig.java
+++ src/main/java/demo/FooConfig.java
@@ -2,6 +2,9 @@ package demo;
 
 import demo.conf.AutowireCandidateResolverConfigurer;
 import demo.conf.MyAutowireCandidateResolver;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.AutowireCandidateResolver;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Configuration
-public class FooConfig {
+public class FooConfig implements BeanFactoryAware{
 
-    @Autowired
-    Environment environment;
+    private Environment environment;
 
     @Bean
     public Foo foo() {
@@ -28,9 +30,13 @@ public class FooConfig {
         return new MyAutowireCandidateResolver();
     }
 
-    // FIXME: After commenting this bean all is working!
     @Bean
     public AutowireCandidateResolverConfigurer autowireCandidateResolverConfigurer() {
         return new AutowireCandidateResolverConfigurer(autowireCandidateResolver());
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        environment = beanFactory.getBean(Environment.class);
     }
 }
